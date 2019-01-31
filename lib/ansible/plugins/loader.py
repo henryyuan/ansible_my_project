1a99f9b0de3439b1de91d7dc6de431e42e473054
henryyuan <yuan870729984@msn.cn>
2019-01-31 18:22:53 +0800

    2018/8/22

--- lib/ansible/plugins/loader.py
+++ lib/ansible/plugins/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:685c3b95bbdefedcc2041f2d92492a8600fcbf65722d6b77d3ee91bad92efcd8
-size 52620
+oid sha256:cfce612efca555125c347a38e1592d5579a67ef7682952cb3470352a813fec58
+size 52781
